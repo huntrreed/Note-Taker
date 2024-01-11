5e377001c8d34e2547462d91771ce97b13580c48
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-01-11 15:18:49 -0700

    added delete function

--- server.js
+++ server.js
@@ -37,13 +37,14 @@ app.post('/api/notes', (req, res) => {
     //requiring a title and text in the note to post the note
     const { title, text } = req.body;
 
-    //if title and text are both there, the new object saves as a new note
+    //if title and text are both there, the new object saves as a new note and gets an ID
     if (title && text) {
         const newNote = {
             title,
             text,
-            id: Date.now()  
+            id: Date.now().toString()
         };
+
 
     //obtain existing notes
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
@@ -77,6 +78,21 @@ app.post('/api/notes', (req, res) => {
       res.status(500).json('Error in posting note');
     }
   });
+
+  app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        notes = notes.filter(note => note.id !== noteId);
+
+        fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
+            if (err) throw err;
+            res.json({ message: 'Note deleted' });
+        });
+    });
+});
 
 //routing everything else to the index.html file 
 app.get('*', (req, res) => {
