2f71f0535c962e2de9da7270a397516eedb6b9fa
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-01-11 14:35:19 -0700

    routing to html files

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ const PORT = 3001;
 const app = express();
 
 //middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//getting files from 'public' folder'
+app.use(express.static('public'));
+
+//routing to the notes file when the notes page is accessed
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+//routing everything else to the index.html file 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 
 app.listen(PORT, () => {
