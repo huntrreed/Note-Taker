863025a75ef93daee06a48e1da2a0563949e35c0
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-01-11 21:08:19 -0700

    updated port for Heroku

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 
 const app = express();
 
